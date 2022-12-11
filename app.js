bc2dafbe0a7809c86b0d7e44b9fee23a25ff75be
Erik Debye <erik.debye@gmail.com>
2022-12-11 09:04:05 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 'use strict';
-
-//Will need following
-//#1. A Customer class containing name, address, phone number, comments
-//      [Initialized with no values]
-//#2. A Pizza class containing size, and toppings (array)
-//#3. An Order Class merging the two. Customer Object imported directly & Pizza objects imported into "pizzas" array
-
 //Sample Structure Below
-
 // Order {
 //     customer : {
 //         name: 'James Rake',
@@ -66,9 +58,16 @@ class Order {
   //   sauce: '',
   // },
   method = '';
+  cost = 0;
+  tax = 0;
 
   updateMethod(method) {
     this.method = method;
+  }
+  updateCustomer(fname, lname, email, comments) {
+    this.name = `${fname} ${lname}`;
+    this.email = email;
+    this.comments = comments;
   }
   updateAddress(street, street2, city, state, zipcode) {
     this.customer.street = street;
@@ -77,16 +76,56 @@ class Order {
     this.customer.state = state;
     this.customer.zipcode = zipcode;
   }
+  addPizza(pizza) {
+    this.items.push(pizza);
+  }
+  updateCost(price) {
+    this.cost = price;
+  }
+  updateTax(tax) {
+    this.tax = tax;
+  }
 }
 
 //controller
 (function () {
-  //save order
-  let order = orderInit();
+  //create order
+  let order = new Order();
+  //grabbing method of delivery
+  order = orderInit(order);
 
   //set up stage 2 (pizza creation)
   document.querySelector('#ready').addEventListener('click', (_) => {
-    createPizza(order);
+    order = createPizza(order);
+  });
+
+  document.querySelector('#form-delivery').addEventListener('submit', (event) => {
+    event.preventDefault();
+    let form = document.querySelector('#form-delivery');
+    const data = new FormData(form);
+    const map = new Map();
+    const entries = data.entries();
+
+    for (const [key, value] of entries) {
+      map.set(key, value);
+    }
+
+    order.updateCustomer(map.get('fname'), map.get('lname'), map.get('email'), map.get('comments'));
+  });
+
+  document.querySelector('#form-pickup').addEventListener('submit', (event) => {
+    event.preventDefault();
+    let form = document.querySelector('#form-pickup');
+    const data = new FormData(form);
+    const map = new Map();
+    const entries = data.entries();
+
+    for (const [key, value] of entries) {
+      map.set(key, value);
+    }
+
+    order.updateAddress(map.get('street'), map.get('street2'), map.get('city'), map.get('state'), map.get('zipcode'));
+    order.updateCustomer(map.get('fname'), map.get('lname'), map.get('email'), map.get('comments'));
   });
 })();
 
@@ -97,28 +136,39 @@ function updateLocalStorage(order) {
   localStorage.setItem('storedOrder', JSON.stringify(order));
 }
 
+function addRemove(action, element) {
+  if (action === 'add') {
+    element.classList.add('hidden');
+  } else {
+    element.classList.remove('hidden');
+  }
+}
+
 /**
  * Fn for stage 1, starting an order
  */
-function orderInit() {
+function orderInit(order) {
   //create order in local storage
 
   //1.Clear previous order
-  localStorage.removeItem('order');
+  localStorage.removeItem('storedOrder');
   //create order
-  let order = new Order();
   //set current order
   updateLocalStorage(order);
 
-  //add event lisener to start btn -> disable btn -> unhide method selection
-  document.querySelector('#startOrder').addEventListener('click', (_) => {
-    document.querySelector('#startOrder').disabled = 'true';
+  const startBtn = document.querySelector('#startOrder');
+  const methods = document.querySelector('#method');
+  const readyBtn = document.querySelector('#ready');
 
-    document.querySelector('#method').classList.remove('hidden');
+  //add event lisener to start btn -> disable btn -> unhide method selection
+  startBtn.addEventListener('click', (_) => {
+    addRemove('add', startBtn);
+
+    addRemove('remove', methods);
   });
 
   //add listener to method
-  document.querySelector('#method').addEventListener('click', (event) => {
+  methods.addEventListener('click', (event) => {
     //firgure out which button they clicked
     const button = event.target.closest('button');
 
@@ -129,11 +179,11 @@ function orderInit() {
       updateLocalStorage(order);
       //if button is showing, hide button
       //if user clicks pickup -> delivery
-      if (!document.querySelector('#ready').classList.contains('hidden')) {
-        document.querySelector('#ready').classList.add('hidden');
+      if (!readyBtn.classList.contains('hidden')) {
+        addRemove('add', readyBtn);
       }
       //show form
-      document.querySelector('#form').classList.remove('hidden');
+      addRemove('remove', document.querySelector('#form'));
 
       //validate form as user types
       form.addEventListener('input', (event) => {
@@ -156,17 +206,21 @@ function orderInit() {
           //use spread
           order.updateAddress(...dataArr);
           updateLocalStorage(order);
-          document.querySelector('#ready').classList.remove('hidden');
+          addRemove('remove', readyBtn);
         }
       });
       //if pickup
     } else if (button.id === 'pickup') {
+      //remove form (if user hits delivery -> pickup)
+      if (!document.querySelector('#form').classList.contains('hidden')) {
+        addRemove('add', document.querySelector('#form'));
+      }
       //update method
       order.updateMethod('pickup');
       //remove address
       updateLocalStorage(order);
       //show button -> ready for stage 2
-      document.querySelector('#ready').classList.remove('hidden');
+      addRemove('remove', readyBtn);
     } else {
       //error handling
       console.log('Hmm there was an error');
@@ -181,16 +235,22 @@ function orderInit() {
  * address only
  */
 function dynamicAdressValidation(event) {
+  //get the target element of the event
   const target = event.target;
 
   if (target.validity.valid === false) {
+    //check if the fieldset already has an error message
     if (!target.closest('fieldset').querySelector('.error')) {
+      //if not, insert an error message
       target
         .closest('fieldset')
         .insertAdjacentHTML('beforeend', `<span class="error">${target.validationMessage}</span>`);
     }
   } else {
-    target.closest('fieldset').querySelector('.error').remove();
+    //if the input is valid & error exist, remove any error messages
+    if (target.closest('fieldset').querySelector('.error')) {
+      target.closest('fieldset').querySelector('.error').remove();
+    }
   }
 }
 
@@ -198,14 +258,15 @@ function dynamicAdressValidation(event) {
  * Validate Address form on submit
  */
 function AddressValidation(form) {
-  //grab all input elements in the form
+  //get all input elements in the form
   const formInputs = form.querySelectorAll('input');
-  //rotate through, return false if not valid
-  formInputs.forEach((input) => {
+
+  //loop over inputs and return false if any input is not valid
+  for (const input of formInputs) {
     if (!input.checkValidity()) {
       return false;
     }
-  });
+  }
 
   //if here, all inputs are valid
   return true;
@@ -213,7 +274,247 @@ function AddressValidation(form) {
 
 /**Create the pizza stage */
 function createPizza(order) {
-  //Empty the page of the init
-  // document.querySelector('#init-wrapper').classList.add('hidden');
-  // document.querySelector('#pizza-wrapper').classList.remove('hidden');
+  //get pizza wrapper and form elements
+  const pizzaWrapper = document.querySelector('#pizza-wrapper');
+  const pizzaForm = document.querySelector('#pizza-form');
+
+  //hide init wrapper and show pizza wrapper
+  addRemove('add', document.querySelector('#init-wrapper'));
+  addRemove('remove', pizzaWrapper);
+
+  //dynamically change cost with inputs
+  pizzaForm.addEventListener('input', (_) => {
+    order = calcCost(order, pizzaForm);
+  });
+
+  //add event listener to form
+  pizzaForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    //validate form
+    if (validatePizza(pizzaForm)) {
+      //get form data and create pizza order
+      const data = new FormData(pizzaForm);
+      const pizza = createPizzaOrder(data);
+      //add pizza to order and update local storage
+      order.addPizza(pizza);
+      updateLocalStorage(order);
+      //reset or checkout
+      order = resetOrCheckout(order);
+      return order;
+    }
+  });
+}
+
+function validatePizza(form) {
+  const sizeEl = form.querySelector('input[name="size"]');
+  const sauceEl = form.querySelector('input[name="sauce"]');
+  const checkedSize = form.querySelector('input[name="size"]:checked');
+  const checkedSauce = form.querySelector('input[name="sauce"]:checked');
+
+  if (checkedSize && checkedSauce) {
+    if (checkedSize) {
+      removeError(sizeEl);
+    }
+    if (checkedSauce) {
+      removeError(sauceEl);
+    }
+    return true;
+  } else {
+    //if size
+    if (!checkedSize) {
+      const fieldset = sizeEl.closest('fieldset');
+      fieldset.style.border = 'solid .2rem red';
+      fieldset.insertAdjacentHTML(
+        'afterbegin',
+        `<p style="color: red; font-size:2rem; padding: 2rem;">Please enter a size!</p>`
+      );
+    }
+    //if sauce
+    if (!checkedSauce) {
+      const fieldset = sauceEl.closest('fieldset');
+      fieldset.style.border = 'solid .2rem red';
+      fieldset.insertAdjacentHTML(
+        'afterbegin',
+        `<p style="color: red; font-size:2rem; padding: 2rem;">Please enter a sauce!</p>`
+      );
+    }
+    return false;
+  }
+}
+
+function removeError(element) {
+  element.closest('fieldset').style.border = 'none';
+  element.closest('fieldset').querySelector('p').remove();
+}
+
+/**
+ * Validate Pizza Form
+ */
+function validatePizza(form) {
+  const checkedSize = form.querySelector('input[name="size"]:checked');
+  const checkedSauce = form.querySelector('input[name="sauce"]:checked');
+
+  if (checkedSize && checkedSauce) {
+    //remove error border
+    document.querySelector('input[name="size"]').closest('fieldset').style.border = 'none';
+    //remove error msg
+    const sizeErr = document.querySelector('input[name="size"]').closest('fieldset').querySelector('p');
+    if (sizeErr) {
+      sizeErr.remove();
+    }
+    //remove error border
+    document.querySelector('input[name="sauce"]').closest('fieldset').style.border = 'none';
+    //remove error msg
+    const sauceErr = document.querySelector('input[name="sauce"]').closest('fieldset').querySelector('p');
+    if (sauceErr) {
+      sauceErr.remove();
+    }
+    return true;
+  } else {
+    //if size
+    if (!checkedSize) {
+      const fieldset = document.querySelector('input[name="size"]').closest('fieldset');
+      fieldset.style.border = 'solid .2rem red';
+      fieldset.insertAdjacentHTML(
+        'afterbegin',
+        `<p style="color: red; font-size:2rem; padding: 2rem;">Please enter a size!</p>`
+      );
+    }
+    //if sauce
+    if (!checkedSauce) {
+      const fieldset = document.querySelector('input[name="sauce"]').closest('fieldset');
+      fieldset.style.border = 'solid .2rem red';
+      fieldset.insertAdjacentHTML(
+        'afterbegin',
+        `<p style="color: red; font-size:2rem; padding: 2rem;">Please enter a sauce!</p>`
+      );
+    }
+    return false;
+  }
+}
+
+/**
+ * Update Pizza Order
+ */
+function createPizzaOrder(data) {
+  //convert FormData to map
+  const dataMap = new Map();
+  //get iterator for entries in data
+  const entries = data.entries();
+
+  //loop over data and add entries to map
+  for (const [key, value] of entries) {
+    if (dataMap.has(key)) {
+      dataMap.get(key).push(value);
+    } else {
+      dataMap.set(key, [value]);
+    }
+  }
+
+  //de-array size and sauce (but keep toppings as an array)
+  dataMap.forEach((value, key) => {
+    if (value.length === 1 && key !== 'toppings') {
+      dataMap.set(key, value[0]);
+    }
+  });
+
+  //convert dataMap into an object
+  const pizza = Object.fromEntries(dataMap);
+  return pizza;
+}
+
+function calcCost(order, form) {
+  //create price & tax
+  let price = 0;
+  let premium = 0;
+  const tax = 0.1;
+  //convert form
+  const pizzaData = new FormData(form);
+  //create pizza (standerizes data)
+  const pizza = createPizzaOrder(pizzaData);
+
+  //calculate cost - size
+  switch (pizza.size) {
+    case '16': {
+      price = 26;
+      premium = 2;
+      break;
+    }
+    case '18': {
+      price += 29;
+      premium = 2.75;
+      break;
+    }
+    case '20': {
+      price += 33;
+      premium = 3.25;
+      break;
+    }
+    default: {
+      price += 38;
+      premium = 3.75;
+      break;
+    }
+  }
+
+  //add toppings if they exists
+  if (pizza.toppings) {
+    pizza.toppings.forEach((topping) => {
+      price += premium;
+    });
+  }
+
+  //update order
+  order.updateCost(price);
+  order.updateTax(price * tax);
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  const priceStr = formatter.format(price);
+  const priceTaxStr = formatter.format(price * 0.1 + price);
+
+  document.querySelector('#price-regular').innerHTML = priceStr;
+  document.querySelector('#price-tax').innerHTML = priceTaxStr;
+
+  updateLocalStorage(order);
+  return order;
+}
+
+function resetOrCheckout(order) {
+  const pizzaCont = document.querySelector('#pizza-wrapper');
+  const checkoutCont = document.querySelector('#checkout-wrapper');
+  const customerCont = document.querySelector('#customer-wrapper');
+
+  //show checkout container and hide pizza container
+  addRemove('add', pizzaCont);
+  addRemove('remove', checkoutCont);
+
+  //add listener to reset button
+  const resetButton = document.querySelector('#resetform');
+  resetButton.addEventListener('click', (_) => {
+    //reset form and hide checkout container
+    document.querySelector('#pizza-form').reset();
+    addRemove('add', checkoutCont);
+    //show pizza container
+    addRemove('remove', pizzaCont);
+  });
+
+  //add listener to checkout button
+  const checkoutButton = document.querySelector('#checkout');
+  checkoutButton.addEventListener('click', (_) => {
+    //hide checkout container and show customer container
+    addRemove('add', checkoutCont);
+    addRemove('remove', customerCont);
+
+    if (order.customer.street && order.customer.city && order.customer.state && order.customer.zipcode) {
+      addRemove('remove', document.querySelector('#form-delivery'));
+    } else {
+      addRemove('remove', document.querySelector('#form-pickup'));
+    }
+
+    return order;
+  });
 }
